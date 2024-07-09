91e8894ec99215d614b9302f08478ce2d4a11cd2
dhimanparas20 <dhimanparas20@gmail.com>
2024-07-09 16:24:14 +0530

    minor fix
    Signed-off-by: dhimanparas20 <dhimanparas20@gmail.com>

--- backend/routing.py
+++ backend/routing.py
@@ -1,6 +1,7 @@
 from django.urls import path,re_path
 from .consumers import AsyncHRTCConsumer,SyncHRTCConsumer
 
+
 websocket_urlpatterns = [
     # path("ws/chat/<str:room_name>/", ChatConsumer.as_asgi()), 
     re_path(r"hrtc/(?P<bus_id>\w+)$", AsyncHRTCConsumer.as_asgi()),
